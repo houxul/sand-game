73180accf1c1f0a5d23118401a80eeacb7afced9
houxul <hou1056105902@gmail.com>
2020-11-23 18:17:01 +0800

    fix databus linearGradient

--- miniprogram/base/databus.js
+++ miniprogram/base/databus.js
@@ -1,5 +1,5 @@
 import Pool from './pool'
-import { genRgb, tryRun } from './utils'
+import { genRgb, tryRun, equalColor } from './utils'
 
 let instance
 
@@ -7,147 +7,160 @@ let instance
  * 全局状态管理器
  */
 export default class DataBus {
-  constructor() {
-    if ( instance )
-      return instance
+	constructor() {
+		if ( instance )
+			return instance
 
-    instance = this
-    this.init()
-  
-    // this.tryRecoveryProgress();
-  }
+		instance = this
+		this.init()
+	
+		// this.tryRecoveryProgress();
+	}
 
-  init() {
-    this.pool = new Pool()
+	init() {
+		this.pool = new Pool()
 
-    const sysInfo = wx.getSystemInfoSync()
-    this.screenWidth  = sysInfo.screenWidth
-    this.screenHeight = sysInfo.screenHeight
+		const sysInfo = wx.getSystemInfoSync()
+		this.screenWidth	= sysInfo.screenWidth
+		this.screenHeight = sysInfo.screenHeight
 
-    this.gameOver   = false
-    this.sandFrame  = 0
-    this.autoDownSandFrame = 0;
-    this.notRepeatColor = true;
-    this.genSandNum = 40;
-    this.overlayAlpha = 0.08;
-    this.pickerRgbs = [];
-    this.pickerLinearGradient = new Array(2000);
-    this.resetPickerRgbs([genRgb(), genRgb(), genRgb(), genRgb()])
-    this.horizontal = false;
-    this.colorChangeSpeed = 50;
-    this.autoDownSand = false;
-    this.bgRgba = [214, 214, 214, 214];
-    this.movementTrack = [];
-    this.myColors = [[[255,0,0]],[[0,255,0]],[[0,0,255]]];
-    this.resetMovementTrack();
+		this.gameOver	 = false
+		this.sandFrame	= 0
+		this.autoDownSandFrame = 0;
+		this.notRepeatColor = true;
+		this.genSandNum = 40;
+		this.overlayAlpha = 0.08;
+		this.pickerRgbs = [];
+		this.linearGradientRgbs = [];
+		this.linearGradientLen = 500;
+		this.resetPickerRgbs([genRgb(), genRgb(), genRgb(), genRgb()])
+		this.horizontal = false;
+		this.colorChangeSpeed = 50;
+		this.autoDownSand = false;
+		this.bgRgba = [214, 214, 214, 214];
+		this.movementTrack = [];
+		this.myColors = [[[255,0,0]],[[0,255,0]],[[0,0,255]]];
+		this.resetMovementTrack();
 
-    this.default = {
-      bgRgba: this.bgRgba,
-    }
+		this.default = {
+			bgRgba: this.bgRgba,
+		}
 
-    this.loadSetting();
-  }
+		this.loadSetting();
+	}
 
-  reset(){
-    this.sandFrame = 0
-    this.autoDownSandFrame = 0;
-  }
+	reset(){
+		this.sandFrame = 0
+		this.autoDownSandFrame = 0;
+	}
 
-  // saveProgress() {
-  //   wx.setStorageSync('databus', {sandFrame: this.sandFrame})
-  // }
+	// saveProgress() {
+	//	 wx.setStorageSync('databus', {sandFrame: this.sandFrame})
+	// }
 
-  // tryRecoveryProgress() {
-  //   const databus = wx.getStorageSync('databus')
-  //   if (databus&&databus.sandFrame) {
-  //     this.sandFrame = databus.sandFrame
-  //   }
-  // }
+	// tryRecoveryProgress() {
+	//	 const databus = wx.getStorageSync('databus')
+	//	 if (databus&&databus.sandFrame) {
+	//		 this.sandFrame = databus.sandFrame
+	//	 }
+	// }
 
-  loadSetting() {
-    const load = (function(key) {
-      const value = wx.getStorageSync('databus.'+key)
-      if (value == "" || value == undefined || value == null) {
-        return;
-      }
-      Reflect.set(this, key, value)
-    }).bind(this);
+	loadSetting() {
+		const load = (function(key) {
+			const value = wx.getStorageSync('databus.'+key)
+			if (value == "" || value == undefined || value == null) {
+				return;
+			}
+			Reflect.set(this, key, value)
+		}).bind(this);
 
-    load('colorChangeSpeed');
-    load('autoDownSand');
-    load('bgRgba');
-    load('genSandNum');
-    load('movementTrack');
-    load('myColors');
-    load('notRepeatColor');
+		load('colorChangeSpeed');
+		load('autoDownSand');
+		load('bgRgba');
+		load('genSandNum');
+		load('movementTrack');
+		load('myColors');
+		load('notRepeatColor');
 
-    // const info = wx.getStorageInfoSync();
-    // console.log('-------------', info);
-  }
+		// const info = wx.getStorageInfoSync();
+		// console.log('-------------', info);
+	}
 
-  updateSetting(options) {
-    for (const item in options) {
-      Reflect.set(this, item, options[item])
-      wx.setStorageSync('databus.' + item, options[item])
-    }
-  }
+	updateSetting(options) {
+		for (const item in options) {
+			Reflect.set(this, item, options[item])
+			wx.setStorageSync('databus.' + item, options[item])
+		}
+	}
 
-  resetPickerRgbs(rgbs) {
-    this.pickerRgbs.splice(0, this.pickerRgbs.length);
-    this.pickerRgbs.push(...rgbs);
+	resetPickerRgbs(rgbs) {
+		this.pickerRgbs.splice(0, this.pickerRgbs.length);
+		this.linearGradientRgbs.splice(0, this.linearGradientRgbs.length);
+		this.pickerRgbs.push(...rgbs);
 
-    this.sandFrame = 0
-    const colors = this.pickerRgbs.concat([this.pickerRgbs[0]]);
-    const step = this.pickerLinearGradient.length/this.pickerRgbs.length
-    for (let i=0; i < this.pickerLinearGradient.length; i++) {
-      const colorIndex = Math.floor(i/step)
-      const percent = (i%step)/step
-      const startColor = colors[colorIndex]
-      const endColor = colors[colorIndex+1]
-      const r = startColor[0] + (endColor[0] - startColor[0]) * percent
-      const g = startColor[1] + (endColor[1] - startColor[1]) * percent
-      const b = startColor[2] + (endColor[2] - startColor[2]) * percent
-      this.pickerLinearGradient[i] = [r, g, b]
-    }
+		this.sandFrame = 0
+		this.filledLinearGradientRgb = rgbs[0];
+		const colors = this.pickerRgbs.concat([this.filledLinearGradientRgb]);
+		this.filledLinearGradientStartIndex = (colors.length - 2) * this.linearGradientLen;
+		for (let i=1; i<colors.length; i++) {
+			this.setLinearGradientRgbs(colors[i-1], colors[i], 
+				this.linearGradientLen, this.linearGradientRgbs, (i-1)*this.linearGradientLen);
+		}
+		tryRun(this.pickerColorChange);
+	}
 
-    tryRun(this.pickerColorChange);
-  }
+	setLinearGradientRgbs(startColor, endColor, linearGradientLen, out, index) {
+		for (let i=0; i<linearGradientLen; i++) {
+			const percent = i/linearGradientLen;
+			const r = startColor[0] + (endColor[0] - startColor[0]) * percent
+			const g = startColor[1] + (endColor[1] - startColor[1]) * percent
+			const b = startColor[2] + (endColor[2] - startColor[2]) * percent
+			out[i+index] = [r, g, b];
+		}
+	}
 
-  get sandFrameColor() {
-    let index = Math.floor(this.sandFrame * this.colorChangeSpeed/1600);
-    if (index < this.pickerLinearGradient.length) {
-      this.sandFrame++
-    } else {
-      index = 0;
-      this.sandFrame = 0;
-      if (this.autoDownSandFrame && this.notRepeatColor) {
-        const colorNum = Math.floor(Math.random()*5)+2;
-        const colors = []
-        for (let i=0; i<colorNum; i++) {
-          colors.push(genRgb())
-        }
-        this.resetPickerRgbs(colors);
-      }
-    }
-    return this.pickerLinearGradient[index];
-  }
+	get sandFrameColor() {
+		let index = Math.floor(this.sandFrame * this.colorChangeSpeed/1600);
+		if (index < this.linearGradientRgbs.length) {
+			this.sandFrame++
+			if (index > this.filledLinearGradientStartIndex && 
+				this.autoDownSandFrame && this.notRepeatColor &&
+				equalColor(this.filledLinearGradientRgb, this.pickerRgbs[0])) {
+					this.filledLinearGradientRgb = genRgb();
+					this.setLinearGradientRgbs(this.pickerRgbs[this.pickerRgbs.length-1], this.filledLinearGradientRgb, 
+						this.linearGradientLen, this.linearGradientRgbs, this.filledLinearGradientStartIndex);
+			}
+		} else {
+			index = 0;
+			this.sandFrame = 0;
+			if (this.autoDownSandFrame && this.notRepeatColor) {
+				const colorNum = Math.floor(Math.random()*5)+1;
+				const colors = [this.filledLinearGradientRgb]
+				for (let i=0; i<colorNum; i++) {
+					colors.push(genRgb())
+				}
+				this.resetPickerRgbs(colors);
+			}
+		}
+		return this.linearGradientRgbs[index];
+	}
 
-  get autoDownSandFramePnt() {
-    if (this.autoDownSandFrame < this.movementTrack.length-1) {
-      this.autoDownSandFrame++;
-    } else {
-      this.autoDownSandFrame = 0;
-    }
-    const track = this.movementTrack[this.autoDownSandFrame];
-    return {x: track[0], y:track[1]};
-  }
+	get autoDownSandFramePnt() {
+		if (this.autoDownSandFrame < this.movementTrack.length-1) {
+			this.autoDownSandFrame++;
+		} else {
+			this.autoDownSandFrame = 0;
+		}
+		const track = this.movementTrack[this.autoDownSandFrame];
+		return {x: track[0], y:track[1]};
+	}
 
-  resetMovementTrack() {
-    this.movementTrack.splice(0, this.movementTrack.length);
-    const arcHeight = this.screenWidth/2;
+	resetMovementTrack() {
+		this.movementTrack.splice(0, this.movementTrack.length);
+		const arcHeight = this.screenWidth/2;
 		for (let i=0; i< this.screenHeight; i++) {
-      this.movementTrack.push([arcHeight * (Math.sin(4*i/arcHeight)+1), i, 1]);
-    }
-    this.movementTrack[0].push(0);
-  }
+			this.movementTrack.push([arcHeight * (Math.sin(4*i/arcHeight)+1), i, 1]);
+		}
+		this.movementTrack[0].push(0);
+	}
 }
