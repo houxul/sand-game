62557adab4a4e9ed8abab49aa1de7a74fd891128
houxul <hou1056105902@gmail.com>
2021-08-06 19:11:57 +0800

    fix cloudfunction addsandpainting

--- cloudfunctions/addsandpainting/index.js
+++ cloudfunctions/addsandpainting/index.js
@@ -4,20 +4,25 @@ const sizeOf = require('image-size');
 const names = require('./name.json');
 const avatars = require('./avatar.json');
 
+const baseIndex = 10003000;
+const baseTime = 1628179200;
+const cellNum = 2;
+
 cloud.init({
 	env: cloud.DYNAMIC_CURRENT_ENV,
 	traceUser: true,
 })
 
 exports.main = async (event, context) => {
-	const sandpaintings = cloud.database().collection('sandpaintings');
-	const cmd = cloud.database().command
-	const {data: [{autoIndex}]} = await sandpaintings.where({autoIndex: cmd.neq(null)}).field({autoIndex: true}).orderBy('autoIndex', 'desc').limit(1).get();
+	console.log('start ~~', new Date().toISOString())
 
-	const count = 2;
-	for (let i=1; i<=count; i++) {
-		const tmpAutoIndex = autoIndex + i;
+	const sandpaintings = cloud.database().collection('sandpaintings');
+	const count = Math.floor(Math.random() * cellNum) + 1;
+	const currentDataIndex = baseIndex + Math.floor((new Date().getTime() / 1000 - baseTime) / (24 * 60 * 60)) * cellNum;
+	for (let i = 1; i <= count; i++) {
+		const tmpAutoIndex = currentDataIndex + i;
 		const url = `https://api.thisissand.com/v2/files/${tmpAutoIndex}/download`;
+		console.log('start download url: ', url)
 		const buffer = await new Promise(function (resolve, reject) {
 			let chunks = [];
 			const res = request(url);
@@ -25,9 +30,10 @@ exports.main = async (event, context) => {
 				chunks.push(chunk);
 			});
 			res.on('end', function () {
-				  resolve(Buffer.concat(chunks));
+				resolve(Buffer.concat(chunks));
 			});
 		})
+		console.log('download finish')
 
 		const id = guid();
 		const cloudPath = id + '.jpg';
@@ -35,10 +41,11 @@ exports.main = async (event, context) => {
 			cloudPath,
 			fileContent: buffer,
 		});
+		console.log('uploadFile success')
 
-		const nameIndex = Math.floor(Math.random()*names.length);
+		const nameIndex = Math.floor(Math.random() * names.length);
 		const avatarIndex = nameIndex % avatars.length;
-		const {width, height} = sizeOf(buffer);
+		const { width, height } = sizeOf(buffer);
 		const data = {
 			_id: id,
 			fileId: fileID,
@@ -47,12 +54,13 @@ exports.main = async (event, context) => {
 			horizontal: width > height,
 			width: width,
 			height: height,
-			likes: Math.floor(Math.random()*999),
-			createdAt: new Date().getTime() - Math.floor(Math.random() * 30*24*60*60*1000),
+			likes: Math.floor(Math.random() * 999),
+			createdAt: new Date().getTime() - Math.floor(Math.random() * 30 * 24 * 60 * 60 * 1000),
 			autoIndex: tmpAutoIndex,
 		};
-		await sandpaintings.add({data});
-		console.log('add success id:', id, tmpAutoIndex);
+		await sandpaintings.add({ data });
+		console.log('add  sandpainting success id:', id, tmpAutoIndex);
+		console.log('end ~~', new Date().toISOString())
 	}
 
 	return true
